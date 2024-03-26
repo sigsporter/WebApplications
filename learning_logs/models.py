a6a9d00ad10d5ea0633e4f1beff790eba04fdabe
Stephen Porter <79169980+sigsporter@users.noreply.github.com>
2024-03-26 07:59:31 -0500

    Moved entry preview out of Meta Class so it works properly. Shoudn't have been part of that class.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -17,6 +17,7 @@ class Entry(models.Model):
 
     class Meta:
         verbose_name_plural = 'entries'
-        def __str__(self):
-            """Return a string representation of the model."""
-            return f"{self.text[:50]}..."
+        
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
