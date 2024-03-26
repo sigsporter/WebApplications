b6bc626b3f27769d27e5b392c8403a71ea4d8db0
Stephen Porter <79169980+sigsporter@users.noreply.github.com>
2024-03-26 08:08:00 -0500

    Updated entry preview to check if the entry is less than 50 characters (preview length) and only display ellipses when the string is longer than 50 characters.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -17,7 +17,10 @@ class Entry(models.Model):
 
     class Meta:
         verbose_name_plural = 'entries'
-        
+
     def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
